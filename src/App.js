a92cf691b0e23c6c1319a1ebeba16b1ad3f8350c
Kartik0899 <jaybudhraja08@gmail.com>
2024-12-19 01:31:04 +0530

    Update tiles color and highlight bgColor

--- src/App.js
+++ src/App.js
@@ -17,6 +17,48 @@ function App() {
     <AppContext.Provider value={providerState}>
       <div className="App">
         <Board />
+        <div
+          style={{
+            padding: "1rem",
+            display: "flex",
+            justifyContent: "flex-start",
+            alignItems: "center",
+            flexDirection: "column",
+          }}
+        >
+          <h1
+            style={{
+              margin: "0",
+              fontSize: "2rem",
+              fontWeight: "bold",
+              color: "#fff",
+            }}
+          >
+            CHESS GAME -
+          </h1>
+          <div
+            style={{
+              fontSize: "larger",
+              color: "#fff",
+              margin: "1rem 0",
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            Player &nbsp;
+            <div
+              style={{
+                display: "inline-block",
+                width: "20px",
+                height: "20px",
+                background: appState.turn === "w" ? "#fff" : "#000",
+                marginRight: "0.5rem",
+                border: "1px solid #739552",
+              }}
+            ></div>
+            make your move
+          </div>
+        </div>
       </div>
     </AppContext.Provider>
   );
